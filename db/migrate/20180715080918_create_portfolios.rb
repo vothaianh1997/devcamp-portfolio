17ce156349a0b90b44e628880bf7aeafa5560f95
vo thai anh <vothaianh1@vos-MacBook-Pro.local>
2018-07-15 15:17:03 +0700

    Added portfolio item via Resource generator

---
+++ db/migrate/20180715080918_create_portfolios.rb
@@ -0,0 +1,14 @@
+class CreatePortfolios < ActiveRecord::Migration[5.2]
+  def change
+    create_table :portfolios do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
