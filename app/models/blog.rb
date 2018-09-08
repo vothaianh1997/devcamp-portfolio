8089f890c1362b508c102745a5f1da65f0f9039c
vo thai anh <vothaianh@vos-MacBook-Pro.local>
2018-09-08 09:59:02 +0700

    Implement validation for blogs, skill, and portfolio items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,8 @@
 class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   default_scope -> { order("id asc") } # desc
-
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates_presence_of :title, :body
 end
