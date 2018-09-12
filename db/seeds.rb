877f9eda3413fdebafa9ce1bcaa333ade6fa2e84
vo thai anh <vothaianh@vos-MacBook-Pro.local>
2018-09-13 01:27:07 +0700

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,6 +68,11 @@ end
   )
 end
 
-
-
 puts "9 portfolio item created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+puts "3 technologies created"
