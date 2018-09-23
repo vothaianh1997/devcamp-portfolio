a82c5e6987c030bcec77a2b809ec6b53ed2723fc
vo thai anh <vothaianh@vos-MacBook-Pro.local>
2018-09-23 21:40:41 +0700

    Installed devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
+#rails generate migration add_email_to_users email:string
 ActiveRecord::Schema.define(version: 2018_09_11_133125) do
 
   # These are extensions that must be enabled in order to support this database
