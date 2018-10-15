92a8ab8898d6be5d69c33accb86610495c013398
vothaianh1997 <vothaianh715@gmail.com>
2018-10-15 23:28:00 +0700

    Implement refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
-      " ".html_safe +
+      "&nbsp&nbsp".html_safe +
       (link_to "Login", new_user_session_path, class: style)
     else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
@@ -60,17 +60,6 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
-  def alerts
-    alert = (flash[:alert] || flash[:error] || flash[:notice])
 
-    if alert
-      alert_generator alert
-    end
-  end
-
-  def alert_generator msg
-    js add_gritter(msg, title: "Jordan Hudgens Portfolio", sticky: false)
-  end
 
 end
-  VtaViewTool::Renderer.copyright 'Vo Thai Anh', 'All right reserved'
